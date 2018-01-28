126aa4b6cd64029f468d73b87c1be2ea5c040a0e
Kevin Herrera <1herrerake@gmail.com>
2018-01-28 17:15:19 -0500

    update jquery

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function() {
+  return <div>Hello world </div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(App);
