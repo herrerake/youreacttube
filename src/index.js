a93091636c70af12d0560c820897f9c80404feb3
Kevin Herrera <1herrerake@gmail.com>
2018-01-28 17:44:34 -0500

    installed youtube api and youtube api search dependency. initial file structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const API_KEY = 'AIzaSyBTVuSbqPACyjQa8btLlp4tgaJwlI-y-W4'
 // Create a new component. This component should produce some html
 
-const App = function() {
+const App = () => {
   return <div>Hello world </div>;
 }
 
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
